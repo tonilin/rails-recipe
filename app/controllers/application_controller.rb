3d6e6a3428a95275b715eb9d784e2025bd0177dc
tonilin <tonilin@gmail.com>
2014-01-29 09:36:17 +0800

    add login and admin check

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,31 @@ class ApplicationController < ActionController::Base
   before_filter :setting_meta_tags
 
 
+  def login_required
+    if current_user.blank?
+      respond_to do |format|
+        format.html  {
+          authenticate_user!
+        }
+        format.js{
+          render :partial => "common/not_logined"
+        }
+        format.all {
+          head(:unauthorized)
+        }
+      end
+    end
+  
+  end
+
+  def require_is_admin
+    unless (current_user && current_user.admin?)
+      redirect_to root_path, :flash => { :error => "no permission" }
+    end
+  end
+
+
+
   private
 
   def setting_meta_tags
